fb7d56aae351d43b3f9e236b4147a9e1ed43cf69
Takamasa Maeda <wilodp@hotmail.com>
2012-03-07 10:17:40 +0900

    test commit

---
+++ app/controllers/hello_controller.php
@@ -0,0 +1,13 @@
+<?php
+
+class HelloController extends AppController {
+
+	public $name = 'Hello';
+	public $uses = null;
+	public $autoLayout = true;
+	public $autoRender = true;
+
+
+	function index(){	}
+
+}
